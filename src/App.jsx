5499cc20a314e41c15f98c25ed173c6c9f7f3333
duclong27 <longofficial27@gmail.com>
2024-12-06 15:13:46 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Silebar from './components/Silebar/Silebar';
+
+const App = ()=> {
+  return (
+    <div>
+<Silebar/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Silebar/Silebar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Silebar.css'
+import InstaLogo from '../../assets/image.png';
+const Silebar = () => {
+  return (
+    <div className='silebar'>
+        <div className="top">
+        <img className='w-20px' src={InstaLogo} alt="Instagram Logo" />
+        </div>
+ 
+ 
+        <div className="bottom">
+        
+  
+  
+        </div>
+        </div>
+  
+)
+
+
+
+}
+
+export default Silebar
